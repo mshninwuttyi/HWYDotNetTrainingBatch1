8d0cb8d33c88fe1f388d11effd360d116cd933c6
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-05-06 23:58:51 +0700

    feat: Add WindowFormApp Project

---
+++ HWYDotNetTrainingBatch1.WinFormsApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace HWYDotNetTrainingBatch1.WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
